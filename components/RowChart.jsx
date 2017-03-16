d764984274909b8d39a217fb290568934d05fc11
Marc Woolfson <marcwoolfson@thedatashed.co.uk>
2017-03-16 16:34:07 +0000

    Added colors and colorAccessor props to RowChart

--- components/RowChart.jsx
+++ components/RowChart.jsx
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import dc from 'dc';
 import { Base } from './Base';
 
-
 class RowChart extends Component {
   static propTypes = {
     elasticX: PropTypes.bool,
@@ -10,13 +9,16 @@ class RowChart extends Component {
     x: PropTypes.func,
     ordinalColors: PropTypes.array,
     onRenderlet: PropTypes.func,
-    onPretransition: PropTypes.func
+    onPretransition: PropTypes.func,
+    colors: PropTypes.func,
+    colorAccessor: PropTypes.func
   };
 
   loadChart = (container) => {
     const chart = dc.rowChart(container);
     const helper = this.props.chartHelper(this, chart);
-    helper.setProperties('elasticX', 'x', 'ordinalColors', 'onRenderlet', 'onPretransition');
+    helper.setProperties('elasticX', 'x', 'ordinalColors', 'onRenderlet', 
+                         'onPretransition', 'colors', 'colorAccessor');
 
     if (this.props.xAxis) {
       this.props.xAxis(chart.xAxis());
